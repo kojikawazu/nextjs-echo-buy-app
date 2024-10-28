52c5684c6d89de08179d155f3796c4b916dc8e33
kojikawazu <nanairo7.daydream21@gmail.com>
2024-10-28 21:49:40 +0900

    アルバムリストのコンポーネント化

--- src/app/(auth)/dashboard/list/page.tsx
+++ src/app/(auth)/dashboard/list/page.tsx
@@ -7,8 +7,7 @@ import { AlbumDataType } from '@/app/types/album-types';
 // utils
 import { fetcher } from '@/app/lib/utils/fetcher';
 // components
-import LogoutBtn from '@/app/components/auth/LogoutBtn';
-import Link from 'next/link';
+import AlbumList from '@/app/components/albums/AlbumList';
 
 /**
  * リストページ
@@ -25,22 +24,7 @@ const ListPage = () => {
 
     return (
         <div className="container mx-auto p-4">
-            <LogoutBtn />
-            <div className="flex justify-center items-center">
-                <h1 className="text-2xl font-bold mb-4">リスト</h1>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {data.map((item) => (
-                    <Link href={`/dashboard/detail/${item.id}`} key={item.id}>
-                        <div className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
-                            <h2 className="text-xl font-semibold mb-2">アルバム {item.id}</h2>
-                            <p className="text-gray-600">{item.title}</p>
-                            <p className="text-sm text-gray-500 mt-2">ユーザーID: {item.userId}</p>
-                        </div>
-                    </Link>
-                ))}
-            </div>
+            <AlbumList data={data} />
         </div>
     );
 };
---
+++ src/app/components/albums/AlbumList.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+
+// types
+import { AlbumDataType } from '@/app/types/album-types';
+// components
+import LogoutBtn from '@/app/components/auth/LogoutBtn';
+
+interface AlbumListProps {
+    data: AlbumDataType[];
+}
+
+/**
+ * アルバムリスト
+ * @param data アルバムデータ
+ * @returns JSX.Element
+ */
+const AlbumList = ({ data }: AlbumListProps) => {
+    return (
+        <>
+            <LogoutBtn />
+
+            <div className="flex justify-center items-center">
+                <h1 className="text-2xl font-bold mb-4">リスト</h1>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {data.map((item) => (
+                    <Link href={`/dashboard/detail/${item.id}`} key={item.id}>
+                        <div className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
+                            <h2 className="text-xl font-semibold mb-2">アルバム {item.id}</h2>
+                            <p className="text-gray-600">{item.title}</p>
+                            <p className="text-sm text-gray-500 mt-2">ユーザーID: {item.userId}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default AlbumList;
