72ce6091c2b483e3a808d34c1bc48eff1a0017ce
kojikawazu <nanairo7.daydream21@gmail.com>
2024-10-28 22:57:32 +0900

    ユーザーIDの非表示

--- src/app/components/albums/AlbumList.tsx
+++ src/app/components/albums/AlbumList.tsx
@@ -28,8 +28,8 @@ const AlbumList = ({ data }: AlbumListProps) => {
                     <Link href={`/albums/detail/${item.id}`} key={item.id}>
                         <div className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
                             <h2 className="text-xl font-semibold mb-2">アルバム {item.id}</h2>
-                            <p className="text-gray-600">{item.title}</p>
-                            <p className="text-sm text-gray-500 mt-2">ユーザーID: {item.userId}</p>
+                            <p className="text-gray-600 mt-2">{item.title}</p>
+                            {/* <p className="text-sm text-gray-500 mt-2">ユーザーID: {item.userId}</p> */}
                         </div>
                     </Link>
                 ))}
